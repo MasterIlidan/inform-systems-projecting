f32dabae72fa37a084f9fd67ab987371d419739b
MasterIlidan <segamil2001@gmail.com>
2024-10-04 16:28:09 +0500

    Попытка починить тесты

--- src/test/java/org/example/utils/JSONReaderTest.java
+++ src/test/java/org/example/utils/JSONReaderTest.java
@@ -3,7 +3,6 @@ package org.example.utils;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
