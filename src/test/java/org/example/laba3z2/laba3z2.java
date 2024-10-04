dc398358e8698e51eb1831f51d101f251ceb230c
sega.m <sega.mezin@gmail.com>
2024-10-04 16:25:13 +0500

    Расчеты для задания 2

--- src/test/java/org/example/laba3z2/laba3z2.java
+++ src/test/java/org/example/laba3z2/laba3z2.java
@@ -49,25 +49,13 @@ public class laba3z2 {
 
     public static void main(String[] args)  {
 
-    double lambda = 1.53;
     int c = 25; //Число целей
     int p = 28; //Кол-во измерений параметра
     int opt = 8; //Кол-во отслеживаемых параметров
     int rpp = 3; //Кол-во расчитываемых параметров по каждой цели
     int in = c * p * opt; //Входные параметры
-       System.out.println("Задание 1");
-       System.out.println("Количество входных параметров: " + in);
     int out = c * rpp; //Выходные параметры
-       System.out.println("Количество выходных параметров: " + out);
     int n2 = in + out; //Суммарное кол-во расчитываемых параметров
-       System.out.println("Суммарное кол-во расчитываемых параметров: " + n2);
-    double V = 0, B = 0;
-    V = (n2 + 2) * ((Math.log(n2 + 2)) / Math.log(2)); //Потенциальный объем программы
-
-       System.out.printf("Потенциальный объем программы: %.2f\n", V);
-    B = Math.pow(V, 2) / (3000 * lambda); //Потенциальное кол-во ошибок
-       System.out.printf("Потенциальное количество ошибок %.2f\n", B);
-
 
        System.out.println("\nЗадание №2");
 
@@ -86,8 +74,8 @@ public class laba3z2 {
        double result5 = countAssamb(result3);
        System.out.println("Количество команд ассемблера: " + result5);
 
-       int chisloprogramistov = 4;
-       int proizvoditelnost = 15;
+       int chisloprogramistov = 5;
+       int proizvoditelnost = 20;
        double result6 = countTime(result3, chisloprogramistov,proizvoditelnost);
        System.out.println("Время программирования (дни): " + result6);
 
